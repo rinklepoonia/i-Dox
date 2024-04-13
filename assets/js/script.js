d9fc65f28ab05175243090cc73f6bf416e561558
rinklepoonia <rinklepoonia889@gmail.com>
2024-04-13 12:52:52 +0530

    collebration-slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,17 @@ menuIcon.addEventListener("click", function () {
     document.body.classList.toggle("overFlowHidden")
     document.querySelector(".menuIcon").classList.toggle("cross")
 })
+$('.slider_collaboration').slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    slidesToShow: 6,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 1000,
+    pauseOnHover: true,
+    cssEase: 'linear',
+});
